b7aec19166fb1ef5759441e746ead5c4c32e18e9
Ephrim Bazile <89447659+kevenbazile@users.noreply.github.com>
2025-01-03 02:34:50 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,176 @@
-!function(){"use strict";marked.setOptions({breaks:!0,highlight:function(e){return Prism.highlight(e,Prism.languages.javascript,"javascript")}});const e=new marked.Renderer;e.link=function(e,t,a){return'<a target="_blank" href="'.concat(e,'">').concat(a,"</a>")};class t extends React.Component{constructor(e){super(e),this.state={markdown:r,editorMaximized:!1,previewMaximized:!1},this.handleChange=this.handleChange.bind(this),this.handleEditorMaximize=this.handleEditorMaximize.bind(this),this.handlePreviewMaximize=this.handlePreviewMaximize.bind(this)}handleChange(e){this.setState({markdown:e.target.value})}handleEditorMaximize(){this.setState({editorMaximized:!this.state.editorMaximized})}handlePreviewMaximize(){this.setState({previewMaximized:!this.state.previewMaximized})}render(){const e=this.state.editorMaximized?["editorWrap maximized","previewWrap hide","fa fa-compress"]:this.state.previewMaximized?["editorWrap hide","previewWrap maximized","fa fa-compress"]:["editorWrap","previewWrap","fa fa-arrows-alt"];return React.createElement("div",null,React.createElement("div",{className:e[0]},React.createElement(a,{icon:e[2],onClick:this.handleEditorMaximize,text:"Editor"}),React.createElement(n,{markdown:this.state.markdown,onChange:this.handleChange})),React.createElement("div",{className:"converter"}),React.createElement("div",{className:e[1]},React.createElement(a,{icon:e[2],onClick:this.handlePreviewMaximize,text:"Previewer"}),React.createElement(i,{markdown:this.state.markdown})))}}const a=e=>React.createElement("div",{className:"toolbar"},React.createElement("i",{className:"fa fa-free-code-camp",title:"no-stack-dub-sack"}),e.text,React.createElement("i",{className:e.icon,onClick:e.onClick})),n=e=>React.createElement("textarea",{id:"editor",onChange:e.onChange,type:"text",value:e.markdown}),i=t=>React.createElement("div",{dangerouslySetInnerHTML:{__html:marked(t.markdown,{renderer:e})},id:"preview"}),r="# Welcome to my React Markdown Previewer!\n\n## This is a sub-heading...\n### And here's some other cool stuff:\n\nHeres some code, `<div></div>`, between 2 backticks.\n\n```\n// this is multi-line code:\n\nfunction anotherExample(firstLine, lastLine) {\n  if (firstLine == '```' && lastLine == '```') {\n    return multiLineCode;\n  }\n}\n```\n\nYou can also make text **bold**... whoa!\nOr _italic_.\nOr... wait for it... **_both!_**\nAnd feel free to go crazy ~~crossing stuff out~~.\n\nThere's also [links](https://www.freecodecamp.org), and\n> Block Quotes!\n\nAnd if you want to get really crazy, even tables:\n\nWild Header | Crazy Header | Another Header?\n------------ | ------------- | -------------\nYour content can | be here, and it | can be here....\nAnd here. | Okay. | I think we get it.\n\n- And of course there are lists.\n  - Some are bulleted.\n     - With different indentation levels.\n        - That look like this.\n\n\n1. And there are numbered lists too.\n1. Use just 1s if you want!\n1. And last but not least, let's not forget embedded images:\n\n![freeCodeCamp Logo](https://cdn.freecodecamp.org/testable-projects-fcc/images/fcc_secondary.svg)\n";ReactDOM.render(React.createElement(t,null),document.getElementById("app"))}();
+(function() {
+  "use strict";
+  
+  // Set options for marked
+  marked.setOptions({
+    breaks: true,
+    highlight: function (code) {
+      return Prism.highlight(code, Prism.languages.javascript, "javascript");
+    }
+  });
+
+  // Create a new marked renderer
+  const renderer = new marked.Renderer();
+  renderer.link = function (href, title, text) {
+    return '<a target="_blank" href="' + href + '">' + text + "</a>";
+  };
+
+  // React component for the app
+  class MarkdownPreviewer extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        markdown: initialMarkdown,
+        editorMaximized: false,
+        previewMaximized: false
+      };
+      this.handleChange = this.handleChange.bind(this);
+      this.handleEditorMaximize = this.handleEditorMaximize.bind(this);
+      this.handlePreviewMaximize = this.handlePreviewMaximize.bind(this);
+    }
+
+    // Handle changes in the editor
+    handleChange(e) {
+      this.setState({
+        markdown: e.target.value
+      });
+    }
+
+    // Toggle editor maximization
+    handleEditorMaximize() {
+      this.setState({
+        editorMaximized: !this.state.editorMaximized
+      });
+    }
+
+    // Toggle preview maximization
+    handlePreviewMaximize() {
+      this.setState({
+        previewMaximized: !this.state.previewMaximized
+      });
+    }
+
+    render() {
+      // Define class names based on maximization states
+      const classNames = this.state.editorMaximized
+        ? ["editorWrap maximized", "previewWrap hide", "fa fa-compress"]
+        : this.state.previewMaximized
+        ? ["editorWrap hide", "previewWrap maximized", "fa fa-compress"]
+        : ["editorWrap", "previewWrap", "fa fa-arrows-alt"];
+
+      return React.createElement(
+        "div",
+        null,
+        React.createElement(
+          "div",
+          { className: classNames[0] },
+          React.createElement(Toolbar, {
+            icon: classNames[2],
+            onClick: this.handleEditorMaximize,
+            text: "Editor"
+          }),
+          React.createElement(Editor, {
+            markdown: this.state.markdown,
+            onChange: this.handleChange
+          })
+        ),
+        React.createElement("div", { className: "converter" }),
+        React.createElement(
+          "div",
+          { className: classNames[1] },
+          React.createElement(Toolbar, {
+            icon: classNames[2],
+            onClick: this.handlePreviewMaximize,
+            text: "Previewer"
+          }),
+          React.createElement(Preview, { markdown: this.state.markdown })
+        )
+      );
+    }
+  }
+
+  // Toolbar component
+  const Toolbar = (props) => React.createElement(
+    "div",
+    { className: "toolbar" },
+    React.createElement("i", {
+      className: "fa fa-free-code-camp",
+      title: "no-stack-dub-sack"
+    }),
+    props.text,
+    React.createElement("i", { className: props.icon, onClick: props.onClick })
+  );
+
+  // Editor component
+  const Editor = (props) => React.createElement(
+    "textarea",
+    {
+      id: "editor",
+      onChange: props.onChange,
+      type: "text",
+      value: props.markdown
+    }
+  );
+
+  // Preview component
+  const Preview = (props) => React.createElement(
+    "div",
+    {
+      dangerouslySetInnerHTML: {
+        __html: marked(props.markdown, { renderer: renderer })
+      },
+      id: "preview"
+    }
+  );
+
+  // Initial Markdown text
+  const initialMarkdown = `# Welcome to my React Markdown Previewer!
+
+## This is a sub-heading...
+### And here's some other cool stuff:
+
+Heres some code, \`<div></div>\`, between 2 backticks.
+
+\`\`\`
+// this is multi-line code:
+
+function anotherExample(firstLine, lastLine) {
+  if (firstLine == '\`\`\`' && lastLine == '\`\`\`') {
+    return multiLineCode;
+  }
+}
+\`\`\`
+
+You can also make text **bold**... whoa!
+Or _italic_.
+Or... wait for it... **_both!_**
+And feel free to go crazy ~~crossing stuff out~~.
+
+There's also [links](https://www.freecodecamp.org), and
+> Block Quotes!
+
+And if you want to get really crazy, even tables:
+
+Wild Header | Crazy Header | Another Header?
+------------ | ------------- | -------------
+Your content can | be here, and it | can be here....
+And here. | Okay. | I think we get it.
+
+- And of course there are lists.
+  - Some are bulleted.
+     - With different indentation levels.
+        - That look like this.
+
+1. And there are numbered lists too.
+1. Use just 1s if you want!
+1. And last but not least, let's not forget embedded images:
+
+![freeCodeCamp Logo](https://cdn.freecodecamp.org/testable-projects-fcc/images/fcc_secondary.svg)
+`;
+
+  // Render the app
+  ReactDOM.render(
+    React.createElement(MarkdownPreviewer, null),
+    document.getElementById("app")
+  );
+})();
